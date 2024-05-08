872d778a3e2b1b0b4bb1cc276760e5d2d65aacd4
mohammedsanaved <mosanaved@gmail.com>
2024-05-08 19:05:53 +0530

    changes in Management

--- src/pages/BoardManagement/BoardManagementStyles.js
+++ src/pages/BoardManagement/BoardManagementStyles.js
@@ -1,63 +1,52 @@
 import styled from "styled-components";
 
 export const BoardManagementStyles = styled.div`
-
-.page-title{
+  .page-title {
     font-family: Inter;
-font-size: 36px;
-font-weight: 700;
-line-height: 43.57px;
-text-align: center;
-color:black;
+    font-size: 36px;
+    font-weight: 700;
+    line-height: 43.57px;
+    text-align: center;
+    color: black;
 
-margin-top:80px;
-margin-bottom:80px;
+    margin-top: 80px;
+    margin-bottom: 80px;
+  }
 
-}
-
-.board-container{
+  .board-container {
     max-width: 1186px;
-    gap:100px;
-}
+    gap: 100px;
+  }
 
-
-
-
-
-.member-name{
+  .member-name {
     font-family: Inter;
-font-size: 24px;
-font-weight: 400;
-line-height: 29.05px;
-text-align: center;
-color:black;
-margin-top:10px;
-margin-bottom:10px;
-
-}
-.member-title{
+    font-size: 24px;
+    font-weight: 400;
+    line-height: 29.05px;
+    text-align: center;
+    color: black;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+  .member-title {
     font-family: Inter;
     font-size: 16px;
     font-weight: 400;
     line-height: 19.36px;
     text-align: center;
-    color:black;
-margin-top:10px;
-margin-bottom:10px;
+    color: black;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
 
-    
-}
+  .board-member-div {
+    max-width: 191px;
+    width: 100%;
+    margin: auto;
+  }
 
-.board-member-div{
-    max-width:191px;
-    width:100%;
-    margin:auto;
-}
-
-
-.member-img{
-    width:100%;
-}
-
-
+  .member-img {
+    width: 90%;
+    aspect-ratio: 3/3;
+  }
 `;
